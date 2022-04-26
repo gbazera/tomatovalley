62c86f5be4012aab1b810280151cea0832f051f5
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2022-04-26 21:19:24 +0400

    Small tweaks

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const nav = document.querySelector('.nav-links');
 const links = nav.querySelectorAll('a');
 
 burger.addEventListener('click', ()=>{
-    burger.classList.toggle('active');
-    nav.classList.toggle('active');
+    ToggleNav();
 });
 
 links.forEach(link => {
     link.addEventListener('click', ()=>{
-        burger.classList.toggle('active');
-        nav.classList.toggle('active');
+        if(nav.classList.contains('active')) ToggleNav();
     });
 });
+
+function ToggleNav(){
+    burger.classList.toggle('active');
+    burger.querySelector('i').classList.toggle('bx-menu');
+    burger.querySelector('i').classList.toggle('bx-x');
+    nav.classList.toggle('active');
+    document.body.classList.toggle('active');
+}
